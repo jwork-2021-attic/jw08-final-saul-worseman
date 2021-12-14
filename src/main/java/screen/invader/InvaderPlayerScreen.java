2f665d4cd6babaa4d67e231dd00fb74f0112f50e
saul-worseman <64306553+saul-worseman@users.noreply.github.com>
2021-12-14 19:02:05 +0800

    initial commit

---
+++ src/main/java/screen/invader/InvaderPlayerScreen.java
@@ -0,0 +1,23 @@
+package screen;
+
+import asciiPanel.AsciiPanel;
+
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+public class InvaderPlayerScreen implements Screen{
+    @Override
+    public void displayOutput(AsciiPanel terminal) {
+
+    }
+
+    @Override
+    public Screen respondToUserInput(KeyEvent key) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Screen nextFrame() {
+        return null;
+    }
+}
